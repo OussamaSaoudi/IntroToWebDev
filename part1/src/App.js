567eccd82ba5c0bf2514d98157e78207db9a8dbd
Oussama Saoudi <oussama.saoudi@ibm.com>
2021-04-11 18:11:16 -0400

    Week 1 Part4

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,72 +1,57 @@
 import React, { useState } from 'react'
-const Button = ({handleClick, text}) => {
-  return (
-      <button onClick={handleClick}>
-        {text}
-      </button>
-  )
-}
-const Statistic = (props) => {
-  return (
-      <tr>
-        <td>{props.text} </td>
-        <td>{props.value}</td>
-      </tr>
-  )
-}
-const Statistics = (props) => {
-  if (props.good + props.neutral + props.bad === 0) {
-    return (
-      <div>
-        <h1>statistics</h1>
-        <div>
-          <p>No feedback given</p>
-        </div>
-      </div>
-    )
-  }
+const Button = ({setter, len}) => {
   return (
     <div>
-      <h1>statistics</h1>
-      <div>
-        <table>
-          <tbody>
-            <Statistic text="good" value={props.good}></Statistic>
-            <Statistic text="neutral" value={props.neutral}></Statistic>
-            <Statistic text="bad" value={props.bad}></Statistic>
-            <tr>
-              <td>all</td>
-              <td>{props.bad + props.neutral + props.good}</td>
-            </tr>
-            <tr>
-              <td>average</td>
-              <td>{(props.good - props.bad)/(props.good+props.bad+props.neutral)}</td>
-            </tr>
-            <tr>
-              <td>positive </td>
-              <td>{100*(props.good)/(props.good+props.bad+props.neutral)} %</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <button onClick={() => setter(Math.floor(Math.random()*len))}>next anecdote</button>
     </div>
   )
 }
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
+const Voter = ({selected, votes, setter}) => {
+  let newVotes = [...votes]
+  newVotes[selected] = newVotes[selected] + 1
   return (
     <div>
-      <h1>give feedback</h1>
-      <div>
-        <Button handleClick={() =>setGood(good+1)} text="good"></Button>
-        <Button handleClick={() =>setNeutral(neutral+1)} text="neutral"></Button>
-        <Button handleClick={() =>setBad(bad+1)} text="bad"></Button>
-      </div>
-      <Statistics good={good} bad={bad} neutral={neutral}></Statistics>
+      <button onClick={() => setter(newVotes)}>vote</button>
+    </div>
+  )
+}
+const Max = ({anecdotes, votes}) => {
+  let index = 0
+  let len = anecdotes.length
+  for (let i = 0; i < len; i++) {
+    if (votes[i] > votes[index]) {
+      index = i
+    }
+  }
+  return (
+    <div>
+      <h1>
+      Anecdote with most votes
+      </h1>
+      <p>{anecdotes[index]}</p>
+    </div>
+
+  )
+}
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+  const [votes, setVote] = useState(new Array(anecdotes.length).fill(0))
+  const [selected, setSelected] = useState(0)
+  console.log(votes)
+  return (
+    <div>
+      {anecdotes[selected]}
+      <p>has {votes[selected]} votes</p>
+      <Button setter={setSelected} len={anecdotes.length} ></Button>
+      <Voter selected={selected} votes={votes} setter={setVote}></Voter>
+      <Max anecdotes={anecdotes} votes={votes}></Max>
     </div>
   )
 }
