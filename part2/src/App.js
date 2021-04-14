7668e09614f338cebc7958444fe69ede0bb9b3cc
Oussama Saoudi <oussama.saoudi@ibm.com>
2021-04-14 06:45:15 -0400

    Week 2a.1

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,14 +1,45 @@
 import React from 'react'
-import Note from './components/Note'
-const App = ({notes}) => (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
-    </div>
+
+const Part = ({part}) => (
+  <li>{part.name}</li>
 )
+const Content = ({partList}) => (
+  <ul>
+    {partList.map(part => 
+      <Part part={part} key={part.id}/> 
+    )}
+  </ul>
+)
+const Course = ({course}) => (
+  <div>
+    <h1>{course.name}</h1>
+    <Content partList={course.parts}/>
+  </div>
+)
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
 
 export default App
