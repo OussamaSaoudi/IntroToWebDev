51e00534238d4fca16e53ea236872f21e35b6b64
Oussama Saoudi <oussama.saoudi@ibm.com>
2021-04-15 06:41:48 -0400

    Week 2c

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,8 +1,23 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import Note from './components/Note'
 
 const App = () => {
-  addNote = event => {
+  const [notes,setNotes] = useState([])
+
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
+  const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
